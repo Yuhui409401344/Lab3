aa5218f1e8ac744b8ab5fb2e3c534517bf5776a7
Yuhui409401344 <101549731+Yuhui409401344@users.noreply.github.com>
2022-12-05 14:50:52 +0800

    Add files via upload

---
+++ test1.php
@@ -0,0 +1,201 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>輸入數值</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-wEmeIV1mKuiNpC+IOBjI7aAzPcEZeedi5yW5f2yOq55WWLwNGmvvx4Um1vskeMj0" crossorigin="anonymous">
+</head>
+
+<body>
+    <style>
+        body{
+            margin:3%;
+        }
+    </style>
+
+    <form method="post" action="test2.php">
+    <table class="table table-hover">
+        <thead>
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">1</th>
+                <th scope="col">2</th>
+                <th scope="col">3</th>
+                <th scope="col">4</th>
+                <th scope="col">5</th>
+                <th scope="col">6</th>
+                <th scope="col">7</th>
+                <th scope="col">8</th>
+                <th scope="col">9</th>
+                <th scope="col">10</th>
+                <th scope="col">11</th>
+                <th scope="col">12</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <th>績優股</th>
+                <td>150</td>
+                <td>127</td>
+                <td>137</td>
+                <td>
+                    <div class="input-group flex-nowrap" style="width:60px">
+                        <input name="data1[]" type="text" class="form-control" aria-label="Username" aria-describedby="addon-wrapping" value="100"  required>
+                    </div>
+                </td>
+                <td>
+                    <div class="input-group flex-nowrap" style="width:60px">
+                        <input name="data1[]" type="text" class="form-control" aria-label="Username" aria-describedby="addon-wrapping" value="100" required>
+                    </div>
+                </td>
+                <td>
+                    <div class="input-group flex-nowrap" style="width:60px">
+                        <input name="data1[]" type="text" class="form-control" aria-label="Username" aria-describedby="addon-wrapping" value="100" required>
+                    </div>
+                </td>
+                <td>
+                    <div class="input-group flex-nowrap" style="width:60px">
+                        <input name="data1[]" type="text" class="form-control" aria-label="Username" aria-describedby="addon-wrapping" value="100" required>
+                    </div>
+                </td>
+                <td>
+                    <div class="input-group flex-nowrap" style="width:60px">
+                        <input name="data1[]" type="text" class="form-control" aria-label="Username" aria-describedby="addon-wrapping" value="100" required>
+                    </div>
+                </td>
+                <td>
+                    <div class="input-group flex-nowrap" style="width:60px">
+                        <input name="data1[]" type="text" class="form-control" aria-label="Username" aria-describedby="addon-wrapping" value="100" required>
+                    </div>
+                </td>
+                <td>
+                    <div class="input-group flex-nowrap" style="width:60px">
+                        <input name="data1[]" type="text" class="form-control" aria-label="Username" aria-describedby="addon-wrapping" value="100" required>
+                    </div>
+                </td>
+                <td>
+                    <div class="input-group flex-nowrap" style="width:60px">
+                        <input name="data1[]" type="text" class="form-control" aria-label="Username" aria-describedby="addon-wrapping" value="100" required>
+                    </div>
+                </td>
+                <td>
+                    <div class="input-group flex-nowrap" style="width:60px">
+                        <input name="data1[]" type="text" class="form-control" aria-label="Username" aria-describedby="addon-wrapping" value="100" required>
+                    </div>
+                </td>
+
+            </tr>
+            <tr>
+                <th>高風險股</th>
+                <td>100</td>
+                <td>90</td>
+                <td>45</td>
+                <td>
+                    <div class="input-group flex-nowrap" style="width:60px">
+                        <input name="data2[]" type="text" class="form-control" aria-label="Username" aria-describedby="addon-wrapping" value="80" required>
+                    </div>
+                </td>
+                <td>
+                    <div class="input-group flex-nowrap" style="width:60px">
+                        <input name="data2[]" type="text" class="form-control" aria-label="Username" aria-describedby="addon-wrapping" value="80" required>
+                    </div>
+                </td>
+                <td>
+                    <div class="input-group flex-nowrap" style="width:60px">
+                        <input name="data2[]" type="text" class="form-control" aria-label="Username" aria-describedby="addon-wrapping" value="80" required>
+                    </div>
+                </td>
+                <td>
+                    <div class="input-group flex-nowrap" style="width:60px">
+                        <input name="data2[]" type="text" class="form-control" aria-label="Username" aria-describedby="addon-wrapping" value="80" required>
+                    </div>
+                </td>
+                <td>
+                    <div class="input-group flex-nowrap" style="width:60px">
+                        <input name="data2[]" type="text" class="form-control" aria-label="Username" aria-describedby="addon-wrapping" value="80" required>
+                    </div>
+                </td>
+                <td>
+                    <div class="input-group flex-nowrap" style="width:60px">
+                        <input name="data2[]" type="text" class="form-control" aria-label="Username" aria-describedby="addon-wrapping" value="80" required>
+                    </div>
+                </td>
+                <td>
+                    <div class="input-group flex-nowrap" style="width:60px">
+                        <input name="data2[]" type="text" class="form-control" aria-label="Username" aria-describedby="addon-wrapping" value="80" required>
+                    </div>
+                </td>
+                <td>
+                    <div class="input-group flex-nowrap" style="width:60px">
+                        <input name="data2[]" type="text" class="form-control" aria-label="Username" aria-describedby="addon-wrapping" value="80" required>
+                    </div>
+                </td>
+                <td>
+                    <div class="input-group flex-nowrap" style="width:60px">
+                        <input name="data2[]" type="text" class="form-control" aria-label="Username" aria-describedby="addon-wrapping" value="80" required>
+                    </div>
+                </td>
+
+            </tr>
+            <tr>
+                <th>潛力股</th>
+                <td>60</td>
+                <td>85</td>
+                <td>90</td>
+                <td>
+                    <div class="input-group flex-nowrap" style="width:60px">
+                        <input name="data3[]" type="text" class="form-control" aria-label="Username" aria-describedby="addon-wrapping" value="50" required>
+                    </div>
+                </td>
+                <td>
+                    <div class="input-group flex-nowrap" style="width:60px">
+                        <input name="data3[]" type="text" class="form-control" aria-label="Username" aria-describedby="addon-wrapping" value="50" required>
+                    </div>
+                </td>
+                <td>
+                    <div class="input-group flex-nowrap" style="width:60px">
+                        <input name="data3[]" type="text" class="form-control" aria-label="Username" aria-describedby="addon-wrapping" value="50" required>
+                    </div>
+                </td>
+                <td>
+                    <div class="input-group flex-nowrap" style="width:60px">
+                        <input name="data3[]" type="text" class="form-control" aria-label="Username" aria-describedby="addon-wrapping" value="50" required>
+                    </div>
+                </td>
+                <td>
+                    <div class="input-group flex-nowrap" style="width:60px">
+                        <input name="data3[]" type="text" class="form-control" aria-label="Username" aria-describedby="addon-wrapping" value="50" required>
+                    </div>
+                </td>
+                <td>
+                    <div class="input-group flex-nowrap" style="width:60px">
+                        <input name="data3[]" type="text" class="form-control" aria-label="Username" aria-describedby="addon-wrapping" value="50" required>
+                    </div>
+                </td>
+                <td>
+                    <div class="input-group flex-nowrap" style="width:60px">
+                        <input name="data3[]" type="text" class="form-control" aria-label="Username" aria-describedby="addon-wrapping" value="50" required>
+                    </div>
+                </td>
+                <td>
+                    <div class="input-group flex-nowrap" style="width:60px">
+                        <input name="data3[]" type="text" class="form-control" aria-label="Username" aria-describedby="addon-wrapping" value="50" required>
+                    </div>
+                </td>
+                <td>
+                    <div class="input-group flex-nowrap" style="width:60px">
+                        <input name="data3[]" type="text" class="form-control" aria-label="Username" aria-describedby="addon-wrapping" value="50" required>
+                    </div>
+                </td>
+            </tr>
+        </tbody>
+    </table>
+    
+    <center><button type="submit" class="btn btn-primary">提交</button></center>
+    </form>
+</body>
+
+</html>
